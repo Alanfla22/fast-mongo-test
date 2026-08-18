637c1e16a7fa54a4109e927b369f573fa790206c
Alanfla22 <114548180+Alanfla22@users.noreply.github.com>
2026-08-18 11:29:46 -0300

    Create api.py

---
+++ app/api.py
@@ -0,0 +1,28 @@
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+import os
+import json
+from fastapi import FastAPI
+
+
+MONGO_KEY = os.environ["MONGO_KEY"]
+MONGO_USER = "jalanfla15_db_user"
+MONGO_APP = "Cluster"
+
+uri = f"mongodb+srv://{MONGO_USER}:{MONGO_KEY}@cluster.eeqmwq8.mongodb.net/?appName={MONGO_APP}"
+
+app = FastAPI()
+
+# listando todos os documentos de uma coleção
+
+@app.get("/")
+def root():
+
+    with MongoClient(uri, server_api=ServerApi('1')) as client:
+
+        db = client.get_database("flash_cards")
+        all_cards = db.cards.find({})
+
+        docs = json.dumps([doc for doc in all_cards], indent=4, default=str, ensure_ascii=False)
+            
+    return docs
