fc70ccd83729418df2f643f2a22f82375f3abc79
Alanfla22 <114548180+Alanfla22@users.noreply.github.com>
2026-08-18 11:31:01 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,5 @@
-from pymongo import MongoClient
-from pymongo.server_api import ServerApi
-import os
-import json
-from fastapi import FastAPI
 import uvicorn
-
-
-MONGO_KEY = os.environ["MONGO_KEY"]
-MONGO_USER = "jalanfla15_db_user"
-MONGO_APP = "Cluster"
-
-uri = f"mongodb+srv://{MONGO_USER}:{MONGO_KEY}@cluster.eeqmwq8.mongodb.net/?appName={MONGO_APP}"
-
-app = FastAPI()
-
-# listando todos os documentos de uma coleção
-
-@app.get("/")
-def root():
-
-    with MongoClient(uri, server_api=ServerApi('1')) as client:
-
-        db = client.get_database("flash_cards")
-        all_cards = db.cards.find({})
-
-        docs = json.dumps([doc for doc in all_cards], indent=4, default=str, ensure_ascii=False)
-            
-    return docs
 
 # This block ensures it only runs if you execute this file directly
 if __name__ == "__main__":
-    uvicorn.run("app.api:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("app.api:app")
