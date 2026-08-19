65522251e520cfbd1776fe05826804a02d5cc2d5
Alanfla22 <114548180+Alanfla22@users.noreply.github.com>
2026-08-19 15:27:23 -0300

    Update api.py

--- app/api.py
+++ app/api.py
@@ -37,7 +37,7 @@ app.add_middleware(
     allow_origins=[origins],
     allow_credentials=True,
     allow_methods=["GET", "POST"],
-    allow_headers=["Content-type"],
+    allow_headers=["application/x-www-form-urlencoded"],
 )
 
 # listando todos os documentos de uma coleção
