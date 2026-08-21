800a33ecbfc88db89a06f59c17ccbc0fb169be05
Alanfla22 <114548180+Alanfla22@users.noreply.github.com>
2026-08-21 14:15:56 -0300

    Update api.py

--- app/api.py
+++ app/api.py
@@ -45,7 +45,7 @@ app.add_middleware(
     allow_origins=["https://flash-card-ycgo.onrender.com", "http://127.0.0.1:5500", "http://127.0.0.1:5500/index.html"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "PATCH"],
-    allow_headers=["application/x-www-form-urlencoded"],
+    allow_headers=["application/json"],
 )
 
 # listando todos os documentos de uma coleção
